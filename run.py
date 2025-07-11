672135b5ae140cdb086f155b904a60e978eae556
r02092 <87572333+r02092@users.noreply.github.com>
2025-07-11 22:23:31 +0900

    カメラに何か写ったら照度差ステレオで法線と反射率を取得するように

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-import base64, dotenv, ftplib, http.server, io, queue, random, os, socketserver, subprocess, threading
+import base64, cv2, dotenv, ftplib, http.server, io, numpy as np, queue, random, os, serial, socketserver, subprocess, threading, time
 
 class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
 	def __init__(self, *args, **kwargs):
@@ -28,10 +28,41 @@ def main():
 	driver = webdriver.Chrome(options=options)
 	driver.get("http://localhost:" + str(PORT))
 	driver.fullscreen_window()
+	ser = serial.Serial("COM3", 9600)
+	cap = cv2.VideoCapture(0)
+	cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2048)
+	cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1536)
+	cap.set(cv2.CAP_PROP_GAMMA, 255)
+	ret, first_frame = cap.read()
+	lut = (np.arange(256) / 255) ** 2.2 * 255
 	dotenv.load_dotenv()
 	ftp = ftplib.FTP(os.getenv("OC2025_FTP_HOSTNAME"))
 	ftp.login(os.getenv("OC2025_FTP_USERNAME"), os.getenv("OC2025_FTP_PASSWORD"))
 	ftp.cwd(os.getenv("OC2025_FTP_DIRECTORY"))
+	while True:
+		ret, frame = cap.read()
+		if np.abs(frame.astype(np.int16) - first_frame.astype(np.int16)).astype(np.uint8).sum() > 100000000:
+			break
+	imgs = []
+	for i in range(4):
+		ser.write(str(i).encode())
+		time.sleep(.3)
+		ret, frame = cap.read()
+		imgs.append(cv2.LUT(frame, lut))
+	imgs = np.array(imgs)
+	imgs = np.clip(imgs[1:] - imgs[0], 0, 255)
+	light = np.load("light.npy")
+	lightT = light.T
+	g = np.tensordot(imgs.transpose(1, 2, 0, 3), (np.linalg.inv(lightT @ light) @ lightT).T, axes=(2, 0))
+	img_albedo = np.linalg.norm(g, axis=3)
+	g_gray = g[:, :, 0] * .0722 + g[:, :, 1] * .7152 + g[:, :, 2] * .2126
+	g_norm = np.linalg.norm(g_gray, axis=2)
+	img_normal = np.zeros(imgs.shape[1:4])
+	mask = g_norm != 0
+	img_normal[mask] = g_gray[mask] / g_norm[mask, None]
+	img_normal = ((img_normal * .5 + .5) * 255).astype(np.uint8)
+	img_albedo = (img_albedo / np.max(img_albedo) * 255).astype(np.uint8)
+	driver.execute_script("document.dispatchEvent(new CustomEvent('predict'))")
 	q.get()
 	filenames = ftp.nlst(".")
 	while True:
@@ -39,6 +70,9 @@ def main():
 		if filename not in filenames:
 			break
 	ftp.storbinary("STOR " + filename, io.BytesIO(driver.get_screenshot_as_png()))
+	cv2.imshow("Normal", img_normal)
+	cv2.imshow("Albedo", img_albedo)
+	cv2.waitKey(0)
 	driver.quit()
 
 if __name__ == "__main__":
