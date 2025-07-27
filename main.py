f5c543e24dc277013c456f2b88626ebc53c5c343
r02092 <87572333+r02092@users.noreply.github.com>
2025-07-27 12:37:28 +0900

    2つの丘の大きさを取得できるように
    掌線の検出は難しかったため

--- main.py
+++ main.py
@@ -53,8 +53,11 @@ def main():
 		ser.write(str(i).encode())
 		time.sleep(.3)
 		ret, frame = cap.read()
+		if i == 0:
+			img_nolight = frame
 		imgs.append(cv2.LUT(frame, lut))
 	imgs = np.array(imgs)
+	img_mask = np.sum(np.abs(img_nolight.astype(np.int16) - first_frame.astype(np.int16)), axis=2) > 127
 	imgs = np.clip(imgs[1:] - imgs[0], 0, 255)
 	light = np.load("light.npy")
 	lightT = light.T
@@ -65,8 +68,34 @@ def main():
 	img_normal = np.zeros(imgs.shape[1:4])
 	mask = g_norm != 0
 	img_normal[mask] = g_gray[mask] / g_norm[mask, None]
+	img_albedo *= img_mask[:, :, None]
+	img_normal *= img_mask[:, :, None]
+	y_tilt = np.nan_to_num(img_normal[:, :, 1] / img_normal[:, :, 0], posinf=1, neginf=-1)
 	img_normal = ((img_normal * .5 + .5) * 255).astype(np.uint8)
 	img_albedo = (img_albedo / np.max(img_albedo) * 255).astype(np.uint8)
+	points = []
+	img_show = img_nolight.copy()
+	for i in range(720):
+		start = np.where(img_mask[:, i])[0]
+		if start.size == 0:
+			continue
+		start = start[0]
+		end = np.where(1 - img_mask[start:, i])[0]
+		if end.size == 0 or end[0] < 9:
+			continue
+		end = end[0] + start
+		valley = np.where(y_tilt[int((start * 3 + end) / 4):int((start + end) / 2), i] > .5)[0]
+		if valley.size == 0 or valley[0] < 9:
+			continue
+		valley = valley[0] + int((start * 3 + end) / 4)
+		points.append([np.sum(np.clip(y_tilt[start:valley], 0, None)), -np.sum(np.clip(y_tilt[valley:end], None, 0))])
+		img_show = cv2.circle(img_show, (i, start), 3, (255, 0, 0), -1)
+		img_show = cv2.circle(img_show, (i, valley), 3, (0, 0, 255), -1)
+		img_show = cv2.circle(img_show, (i, end), 3, (0, 255, 0), -1)
+	points = np.array(points)
+	print(np.mean(points, axis=0))
+	cv2.imshow("Show", img_show)
+	cv2.waitKey(0)
 	driver.execute_script("document.dispatchEvent(new CustomEvent('predict'))")
 	q.get()
 	filenames = ftp.nlst(".")
@@ -75,9 +104,6 @@ def main():
 		if filename not in filenames:
 			break
 	ftp.storbinary("STOR " + filename, io.BytesIO(driver.get_screenshot_as_png()))
-	cv2.imshow("Normal", img_normal)
-	cv2.imshow("Albedo", img_albedo)
-	cv2.waitKey(0)
 	driver.quit()
 
 if __name__ == "__main__":
